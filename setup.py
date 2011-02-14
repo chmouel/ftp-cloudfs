9b706363aa6a63b9f9b6aa42585747d7e8abb7d3
Chmouel Boudjnah <chmouel.boudjnah@rackspace.co.uk>
2011-02-14 11:43:55 +0000

    Distutils support.
    Allow us to upload it directly to pypi http://pypi.python.org/pypi/ftp-cloudfs/

--- setup.py
+++ setup.py
@@ -1,14 +1,33 @@
 #!/usr/bin/env python
-
-from distutils.core import setup
+import os
+from setuptools import setup, find_packages
 from ftpcloudfs.constants import version
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='ftp-cloudfs',
       version=version,
+      download_url="https://github.com/chmouel/ftp-cloudfs/zipball/%s" % (version),
       description='FTP interface to Rackspace Cloud Files',
       author='Chmouel Boudjnah',
       author_email='chmouel.boudjnah@rackspace.co.uk',
-      url='http://blog.chmouel.com',
-      packages=['ftpcloudfs'],
-      scripts=['bin/ftpcloudfs']
-     )
+      url='https://github.com/chmouel/ftp-cloudfs',
+      long_description = read('README.rst'),
+      license='MIT',
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['pyftpdlib'],
+      scripts=['bin/ftpcloudfs'],
+      packages = find_packages(exclude=['tests', 'debian']),
+      tests_require = ["nose"],
+      classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Environment :: No Input/Output (Daemon)',
+        'License :: OSI Approved :: BSD License',
+        ],
+      test_suite = "nose.collector",
+      )
