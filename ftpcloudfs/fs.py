ddbc4ad661349f4e35354aae81c9b2fe18da7756
Juan J. Martinez <juan@memset.com>
2011-07-25 13:25:03 +0100

    Allow direct access to a container even if container listing is not allowed.
    This allows the implementation of container ACLs.

--- ftpcloudfs/fs.py
+++ ftpcloudfs/fs.py
@@ -250,7 +250,18 @@ class ListDirCache(object):
                 stat_info = self.cache[leaf]
             except KeyError:
                 logging.debug("Didn't find %r in directory listing" % leaf)
-                raise IOSError(ENOENT, 'No such file or directory %s' % leaf)
+                # it can be a container and the user doesn't have
+                # permissions to list the root
+                if directory == '/' and leaf:
+                    try:
+                        container = self.cffs.connection.get_container(leaf)
+                    except cloudfiles.errors.ResponseError:
+                        raise IOSError(ENOENT, 'No such file or directory %s' % leaf)
+
+                    logging.debug("Accessing %r container without root listing" % leaf)
+                    stat_info = self._make_stat(count=container.object_count, bytes=container.size_used)
+                else:
+                    raise IOSError(ENOENT, 'No such file or directory %s' % leaf)
         else:
             # Root directory size is sum of containers, count is containers
             bytes = sum(stat_info.st_size for stat_info in self.cache.values())
