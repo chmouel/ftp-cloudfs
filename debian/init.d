83e0443fba33cb6f2745531f1664784ca245a686
Christophe Le Guern <c35sys@gmail.com>
2011-11-25 13:06:29 +0100

    - fix README.source with $VERSION and fix a typo - fix /var/run/ftp-cloudfs in init.d (it is in tmpfs) - Modify changelog with 0.10 version

--- debian/init.d
+++ debian/init.d
@@ -20,7 +20,8 @@ DAEMON=/usr/bin/ftpcloudfs      # Introduce the server's location here
 LOGDIR=/var/log/${NAME}         # Logs directory
 LOGFILE=${LOGDIR}/${NAME}.log   # Log file
 DAEMON_ARGS="-l ${LOGFILE}"     # Arguments to run the daemon with
-PIDFILE=/var/run/${NAME}/$NAME.pid
+PIDDIR=/var/run/${NAME}
+PIDFILE=${PIDDIR}/$NAME.pid
 SCRIPTNAME=/etc/init.d/$NAME
 
 # Exit if the package is not installed
@@ -44,6 +45,12 @@ if [ ! -f ${CONFIGFILE} ]; then echo "Missing Configuration file"; exit 0; fi
 #
 do_start()
 {
+	# create PID dir (/var/run/ is tmpfs)
+	if [ ! -d ${PIDDIR} ]
+	then
+	        mkdir -m 0700 -p ${PIDDIR}
+       		chown -R ftpcloudfs:ftpcloudfs ${PIDDIR}
+	fi
 	# create PID file
 	touch ${PIDFILE}
 	chown ftpcloudfs:ftpcloudfs ${PIDFILE}
