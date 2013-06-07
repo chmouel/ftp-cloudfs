e68b12eb066468d4f3f27eecfb98fd859c5b0b86
Juan J. Martinez <juan@memset.com>
2013-06-07 15:24:06 +0100

    deprecated token (not used anymore)

--- ftpcloudfs/constants.py
+++ ftpcloudfs/constants.py
@@ -4,7 +4,6 @@ default_banner = "Rackspace Cloud Files %v using pyftpdlib %f ready."
 default_config_file = '/etc/ftpcloudfs.conf'
 default_address = '127.0.0.1'
 default_port = 2021
-default_workers = 1
 cloudfiles_api_timeout = 30
 
 # keystone defaults
